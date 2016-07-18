06c20c1b46a58b37c27b41362e5f17ee51363872
buc1993 <bharatchoudhary93@gmail.com>
2016-07-19 01:38:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,6 @@
     <img src="Redfort.jpg" style="width:240px;height:150px;">
     <p>A red sandstone structure which presents India before the world, Red Fort in Delhi is rightly been called the pride of the city and of India too.</p>
     <p>Inscribed as a World Heritage Site by UNESCO, this magnificent red citadel is but just a ghost of its former glory days. Red Fort in Delhi was built by the Mughal Emperor Shahjahan in the 17th century AD.</p>
-    <p>It took around 10 years to complete and is originally referred to as ‘Quila-e-Mubarak’ as royal family used to live there. Inside the fort, there are public and private halls, mosques, gardens and alongside this runs the river Yamuna.</p>
 </div>
 
 <div class="fort">
@@ -38,7 +37,6 @@
     <img src="Amber-Fort.jpg" style="width:240px;height:150px;">
     <p>A beautiful gemstone in the royal crown of Rajasthan, Jaipur charms the guests with its fleet of palaces and forts. One of the most visited and beguiling of Jaipur forts is the Amber Fort, situated in a serene backdrop of the Aravali Hill.</p>
     <p>When you reach the fort, you will get a warm welcome by the silent wavelets of the Maota Lake. The blend of Hindu, Rajput and Mughal architectural style adorn the fort and make the guests let them loose to its grandeur of presentation.</p>
-    <p>The intricate marble, glass, metal and wood carvings can be well witnessed in this beautiful fort built by Raja Man Singh.</p>
 </div>
 
 <div class="fort">
@@ -46,7 +44,6 @@
     <img src="Agra-Fort.jpg" style="width:240px;height:150px;">
     <p>Though the city of Agra is most famous for its iconic Taj Mahal, but there are several other architectural wonders that still make it one of the best destination for a cultural tour to India.</p>
     <p>The red giant structure of Agra fort speaks volumes about the Mughal dynasty, their rise and fall in the country of India. While you enter inside this fort, a beautifully manicured lawn gives you a green carpet welcome.</p>
-    <p>There are different palaces inside this red citadel most of which were built by the Mughal Emperor Shah Jahan. This chivalric king also had spent his last years in a high towering palace of Red Fort from where Taj Mahal can be seen clearly.</p>
 </div>
 
 <div class="fort">
@@ -54,7 +51,6 @@
     <img src="Daulatabad-Fort.jpg" style="width:240px;height:150px;">
     <p>Though the sand of time has left its imprints behind the Daulatabad Fort, it is still can be called one of the best documents containing the history of the city.</p>
     <p>The fort overlooks the green views of the vegetation around and is certainly a photographer’s paradise. With its vintage look, it never ceases to amaze the visitors.</p>
-    <p>It’s been ruled by many dynasties and their remains can be still seen in this almost forgotten fort of Aurangabad.</p>
 </div>
 
 <div class="fort">

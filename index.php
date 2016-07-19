5cffe0bf15e063d9ef30669e52d612e7329763bb
buc1993 <bharatchoudhary93@gmail.com>
2016-07-19 23:17:28 +0530

    Update and rename demo.html to index.php

--- index.php
+++ index.php
@@ -18,16 +18,16 @@
       <h2>POPULAR FORTS</h2></center>
   </div>
   <div class="innerright" style="max-width:800px">
-    <img class="mySlides" src="img/Redfort.jpg" style="width:100%;height:210px;">
-    <img class="mySlides" src="img/Amber-Fort.jpg" style="width:100%;height:210px;">
-    <img class="mySlides" src="img/Agra-Fort.jpg" style="width:100%;height:210px;">
-    <img class="mySlides" src="img/jaisalmer-fort.jpg" style="width:100%;height:210px;">
-    <img class="mySlides" src="img/Golconda-Fort-hyderabad.jpg" style="width:100%;height:210px;">
-    <img class="mySlides" src="img/junagarh-fort.jpg" style="width:100%;height:210px;">
-    <img class="mySlides" src="img/gwalior-fort.jpg" style="width:100%;height:210px;">
-    <img class="mySlides" src="img/jhansi-fort.jpg" style="width:100%;height:210px;">
-    <img class="mySlides" src="img/chittorgarh-fort.jpg" style="width:100%;height:210px;">
-    <img class="mySlides" src="img/kangra-fort.jpg" style="width:100%;height:210px;">
+    <img class="mySlides" src="Redfort.jpg" style="width:100%;height:210px;">
+    <img class="mySlides" src="Amber-Fort.jpg" style="width:100%;height:210px;">
+    <img class="mySlides" src="Agra-Fort.jpg" style="width:100%;height:210px;">
+    <img class="mySlides" src="jaisalmer-fort.jpg" style="width:100%;height:210px;">
+    <img class="mySlides" src="Golconda-Fort-hyderabad.jpg" style="width:100%;height:210px;">
+    <img class="mySlides" src="junagarh-fort.jpg" style="width:100%;height:210px;">
+    <img class="mySlides" src="gwalior-fort.jpg" style="width:100%;height:210px;">
+    <img class="mySlides" src="jhansi-fort.jpg" style="width:100%;height:210px;">
+    <img class="mySlides" src="chittorgarh-fort.jpg" style="width:100%;height:210px;">
+    <img class="mySlides" src="kangra-fort.jpg" style="width:100%;height:210px;">
   </div>
 </div>
 
@@ -56,61 +56,61 @@
 
 <div class="fort">
     <h2><center>1) Red Fort, Delhi</center></h2>
-    <center><img src="img/Redfort.jpg"  style="width:240px;height:150px;"></center>
+    <center><img src="Redfort.jpg"  style="width:240px;height:150px;"></center>
     <p>The Red Fort (Lal Qila) is one of Delhi’s best tourism sights. An amazing red sandstone fort built by the Mughal Emperor Shah Jahan as his ruling palace. Completed in 1648, the years since haven’t treated the buildings kindly: the rooms have very long since been stripped of most objects, the marble inlays are gone and several buildings are off limitations.</p>
 </div>
 
 <div class="fort">
     <h2><center>2) Amber Fort,  Rajasthan</center></h2>
-    <center><img src="img/Amber-Fort.jpg" style="width:240px;height:150px;"></center>
+    <center><img src="Amber-Fort.jpg" style="width:240px;height:150px;"></center>
     <p>Amber Fort gets its name through the small historical town of Amber (also called Amer) that’s located around 20 minutes from the Pink City of Jaipur. The Fort’s primary building was started in 1592 by Rajput ruler Maharaja Man Singh. Today, |it is one of Jaipur’s best tourer places. The Fort’s building is a wonderful blend of Hindu and Mughal influences. You can find out about the Fort’s history in the evening sound and lighting show. Another of the Fort’s great advantages is the opportunity to ride an elephant up to its entrance. These elephant rides are only available in the mornings, until 11.30 a.m., though.</p>
 </div>
 
 <div class="fort">
     <h2><center>3) Agra Fort, Agra</center></h2>
-    <center><img src="img/Agra-Fort.jpg"  style="width:240px;height:150px;"></center>
+    <center><img src="Agra-Fort.jpg"  style="width:240px;height:150px;"></center>
     <p>It is the most significant fort in India located in Agra Uttar Pradesh, about 2Km form it’s more popular sister monument, “The Taj Mahal”. Sikandar Lodi was the 1st Sultan of Delhi who moved to Agra and lived in the fort. Agra Fort also called the red fort of Agra is one of the ideal tourist spots in Uttar Pradesh. The red sandstone building was completed in 1573.</p>
 </div>
 
 <div class="fort">
     <h2><center>4) Jaisalmer Fort, Rajasthan</center></h2>
-    <center><img src="img/jaisalmer-fort.jpg"  style="width:240px;height:150px;"></center>
+    <center><img src="jaisalmer-fort.jpg"  style="width:240px;height:150px;"></center>
     <p>Jaisalmer fort, one of the most significant forts on the globe, located in the state of Rajasthan, built by Rawal Jaisal. The spectacular foundation of Jaisalmer Fort is located in the midst of the sandy terrain of the Fantastic Thar Desert. Jaisalmer fort is also called Sonar killa and “Golden Fort. Main sights inside the forts are Jain temples, Royal palace and big gates. Jaisalmer is the known for its Trikuta Hill, palace, Havelis and Lakes.</p>
 </div>
 
 <div class="fort">
     <h2><center>5) Golkonda Fort, Hyderabad</center></h2>
-    <center><img src="img/Golconda-Fort-hyderabad.jpg"  style="width:240px;height:150px;"></center>
+    <center><img src="Golconda-Fort-hyderabad.jpg"  style="width:240px;height:150px;"></center>
     <p>The greatest Golconda fort situated at Hyderabad, Andhra Pradesh, built by Kakatiya kings. The historical monument is famous for its rich history and a majestic and imposing great structure. Golconda was once famous for the Wonderful diamonds found on the south-east at Kollur Mine nearby Kollur Lake. The amazing Golconda Fort is known for its military architecture and one of the very most fantastic monuments in India. The Fort’s ruins include several gateways, temples, drawbridges, royal apartments, halls, and stables. Several of its bastions are still mounted with canons. What’s most fascinating regarding the Fort though, is its buildings and special acoustic design.</p>
 </div>
 
 <div class="fort">
     <h2><center>6) Junagarh Fort, Rajasthan</center></h2>
-    <center><img src="img/junagarh-fort.jpg"  style="width:240px;height:150px;"></center>
+    <center><img src="junagarh-fort.jpg"  style="width:240px;height:150px;"></center>
     <p>Junagarh Fort is a fort in the city of Bikaner, Rajasthan, India. The fort was originally named Chintamani and was renamed Junagarh or “Old Fort” in the early 20th century when the ruling family shifted to Lalgarh Palace outside of the fort restricts. It is mostly of the significant forts in Rajasthan that is not built on the hilltop. The current city of Bikaner has developed across the fort. The Junagarh fort of Rajasthan is a solid structure encircled by a moatand it has several amazing palaces within. This fort is the real reflection of the historic structures of this place and was built by Raja Rai Singh. You will find thirty seven palaces, pavilions and several temples in the fort.</p>
 </div>
 
 <div class="fort">
     <h2><center>7) Gwalior Fort, Madhya Pradesh</center></h2>
-    <center><img src="img/gwalior-fort.jpg"  style="width:240px;height:150px;"></center>
+    <center><img src="gwalior-fort.jpg"  style="width:240px;height:150px;"></center>
     <p>Gwalior Fort the gorgeous famous monument situated in Madhya Pradesh constructed by Rana Man Singh Tomar. This wonderful monument holds an example of beauty of the amazing ancient Indian architecture. The top attractions of Gwalior fort are popular Sas-Bahu temple, Gujari Mahal that is now a museum and The Man Mandir. The fascinating monument is among the largest forts in India. Madhya Pradesh the Heartland of India ranks one of the most famous sightseeing attractions in India for its |excellent historical Indian architecture.</p>
 </div>
 
 <div class="fort">
     <h2><center>8) Jhansi Fort, Uttar Pradesh</center></h2>
-    <center><img src="img/jhansi-fort.jpg"  style="width:240px;height:150px;"></center>
+    <center><img src="jhansi-fort.jpg"  style="width:240px;height:150px;"></center>
     <p>The historical fort at Jhansi, popularly named ‘Rani Jhansi ka Quila’, is a located in Uttar Pradesh, North India. Jhansi Fort was constructed by Bir Singh Ju Deo, the Raja of Orchha, in 1613. It is located on the rocky hill named Bangra in the town of Balwantnagar, known these days as Jhansi. The fort has ten gates including the Khandero Gate, Chand Gate, Datia Darwaza, Sainyar Gate, Unnao Gate, Orcha Gate, Jharna Gate, Sagar Gate and Laxmi Gate. The fort has a great collection of sculptures that provide a fantastic insight into the lively history of Bundelkhand. Among tourist destinations within the main fort region are the Karak Bijli Toup, Shiv temple and a Mazar of Ghulam Gaus Khan, Rani Jhansi Garden, Moti Bai and Khuda Baksh.</p>
 </div>
 
 <div class="fort">
     <h2><center>9) Chittorgarh Fort,  Rajasthan</center></h2>
-    <center><img src="img/chittorgarh-fort.jpg"  style="width:240px;height:150px;"></center>
+    <center><img src="chittorgarh-fort.jpg"  style="width:240px;height:150px;"></center>
     <p>Chittorgarh Fort is the biggest fort of Rajasthan, India. It is also known as Water Fort, The fort had 84 water bodies within, out of that only about 22 are available today. This lifestyle testimony signifies the braveness of Maharana Pratap Singh and the superb Rajput rulers of Rajasthan. The most important forts in Rajasthan, host the greatest Rajput festival called the “Jauhar Mela”. The most significant monuments in the Chittorgarh Fort are Gaumukh reservoir, Rana Kumbha palace and Vijay Stambha.</p>
 </div>
 
 <div class="fort">
     <h2><center>10) Kangra Fort,  Himachal Pradesh</center></h2>
-    <center><img src="img/kangra-fort.jpg"  style="width:240px;height:150px;"></center>
+    <center><img src="kangra-fort.jpg"  style="width:240px;height:150px;"></center>
     <p>The popular Kangra fort built by the Royal family of Kangra, located on the confluence of the Banganga and Manjhi Rivers within the Kangra valley of Himachal Pradesh. The popular monumental fort is count among the older forts across the world. It is the most popular fort in the Himalayas and the best forts in India, The best significant monuments in the Kangra Fort is Vajreshwari temple. The most amazing way to see Kangra Fort by the road during travelling. The popular amazing attractions of Himachal Pradesh are very near to popular Dharamsala.</p>
 </div>
 
